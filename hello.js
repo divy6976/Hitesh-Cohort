0bc84334d4c3973a9e7145b3efea84dc1f126e04
divy6976 <divyprakashpandey6@gmail.com>
2025-07-29 10:40:50 +0530

    1st lecture

---
+++ hello.js
@@ -0,0 +1,31 @@
+// function printChai(){
+//     console.log("Hello Divy")
+// }
+
+// function brush(cnt){
+//     console.log(`Brush count: ${cnt}`)
+
+// }
+
+//  function add(a,b){
+//     return a+b;
+//  }
+
+
+
+
+// printChai();
+// brush(69);
+
+
+// console.group(add(5,6))
+
+
+function total (tray1,tray2){
+    return tray1 * tray2;
+
+}
+
+
+
+console.log(total(12,20));
